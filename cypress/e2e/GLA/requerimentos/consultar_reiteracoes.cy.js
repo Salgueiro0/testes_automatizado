20ec76fcba6d84042e587a6c9b1bffe123f51530
vitoria.gomes <vitoria.gomes@engesoftware.com.br>
2025-07-22 16:01:29 -0300

    Consultar reiterações

--- cypress/e2e/GLA/requerimentos/consultar_reiteracoes.cy.js
+++ cypress/e2e/GLA/requerimentos/consultar_reiteracoes.cy.js
@@ -41,7 +41,7 @@ describe('consultar_requerimentos', () => {
         cy.get('#aa_processo_sei').type('1111')
         cy.get('#select2-tp_empreendimento-container').click()
         cy.get('.select2-results').find('li').contains('Parcelamento de Solo Urbano - Novo').click({force: true})
-        cy.get('#ds_empreendimento').type('esmeralda-teste')
+        cy.get('#ds_empreendimento').type('Ambiental11')
         cy.get('#select2-tp_prioridade-container').click()
         cy.get('.select2-results').find('li').contains('Planejamento Estratégico ').click({force: true})
         cy.get('#select2-cd_pessoa_titular-container').click()
@@ -60,20 +60,20 @@ describe('consultar_requerimentos', () => {
         cy.get('.sidebar-menu > :nth-child(2) > a > span').click()
 
         cy.get('.col-md-6 > .form-group > .select2-container > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').click()
-        cy.get('.select2-results').find('li').contains('esmeralda-teste').click({ force: true })
+        cy.get('.select2-results').find('li').contains('Ambiental11').click({ force: true })
         cy.get('#pesquisar').click()
         cy.get('a[title="Visualizar"]').first().click();
 
 
         //CRIAR PRIMEIRO REQUERIMENTO PARA TESTE
-
-        cy.get('.requerimentos-aba').click()
+        cy.wait(2000)
+        cy.get('.requerimentos-aba').should('be.visible').and('contain.text', 'Requerimento').click()
 
         //Cadastro
         cy.get('#requerimento > .box > .box-body > .panel > .panel-body > .panel-footer > .btn-toolbar > .btn-success').click()
 
         cy.get('[id^="select2-tp_demanda_permissao-"][id$="-container"]').click();
-        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('LO - Licença de Operação ').click({ force: true });
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('LP - Licença Prévia').click({ force: true });
 
         cy.get('#nr_oficio').type('2000')
 
@@ -105,10 +105,12 @@ describe('consultar_requerimentos', () => {
         cy.wait(1000)
         cy.get('#nr_doc_sei_despacho').type('1111')
 
-        cy.get('#submit').click()
+        cy.get('#submit').should('be.visible').and('contain.text', 'Salvar').click()
+        cy.wait(1000)
         cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Continuar').click()
+        cy.wait(1000)
         cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click()
-
+        cy.wait(1000)
 
         //CRIAR SEGUNDO REQUERIMENTO PARA TESTE
 
@@ -117,8 +119,9 @@ describe('consultar_requerimentos', () => {
         cy.get('.sidebar-menu > :nth-child(2) > a > span').click()
 
         cy.get('.col-md-6 > .form-group > .select2-container > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').click()
-        cy.get('.select2-results').find('li').contains('esmeralda-teste').click({ force: true })
+        cy.get('.select2-results').find('li').contains('Ambiental11').click({ force: true })
         cy.get('#pesquisar').click()
+        cy.wait(2000)
         cy.get('a[title="Visualizar"]').first().click();
 
         cy.get('.requerimentos-aba').click()
@@ -129,39 +132,44 @@ describe('consultar_requerimentos', () => {
         cy.get('[id^="select2-tp_demanda_permissao-"][id$="-container"]').click();
         cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('LP - Licença Prévia').click({ force: true });
 
-        cy.get('#nr_oficio').type('2000')
+        cy.get('#nr_oficio').type('2')
 
-        cy.get('#aa_oficio').type('2025')
+        cy.get('#aa_oficio').type('2024')
 
         cy.get('[id^="select2-cd_orgao_oficio-"][id$="-container"]').click();
-        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('ACJUR - ADVOCACIA E CONSULTORIA JURIDICA').click({ force: true });
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('ADHAR - ASSESSORIA DA DIRETORIA DE HABITAÇÃO E DE REGULARIZAÇÃO').click({ force: true });
 
-        cy.get('#ds_observacao_doc_oficio').type('20888')
+        cy.get('#ds_observacao_doc_oficio').type('208')
 
         cy.get('#select2-cd_pessoa_titular-container').click()
-        cy.get('.select2-results').find('li').contains('ALLAN GUIMARAES DIOGENES').click({ force: true })
+        cy.get('.select2-results').find('li').contains(' Camila da Fonseca Vieira ').click({ force: true })
 
-        cy.get('#ds_requerimento').type('Fazenda Graça')
+        cy.get('#ds_requerimento').type('Fazenda')
 
         cy.get('[id^="select2-cd_orgao_requerimento-"][id$="-container"]').click();
-        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('ANATEL - AGÊNCIA NACIONAL DE TELECOMUNICAÇÕES').click({ force: true });
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('CAIXA - CAIXA ECONÔMICA FEDERAL').click({ force: true });
 
-        cy.get('#dt_publicacao').type('2020-01-01')
+        cy.get('#dt_publicacao').type('2025-01-01')
 
         cy.get('[id^="select2-tp_status-"][id$="-container"]').click();
-        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('Deferido').click({ force: true });
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('A protocolar').click({ force: true });
 
-        cy.get('#cd_orgao_processo_sei').type('1')
-        cy.get('#nr_processo_sei').type('1')
-        cy.get('#aa_processo_sei').type('1')
+        cy.get('#cd_orgao_processo_sei').type('2')
+        cy.get('#nr_processo_sei').type('2')
+        cy.get('#aa_processo_sei').type('2')
 
         cy.get('#fl_reserva_orcamentaria').click()
         cy.wait(1000)
-        cy.get('#nr_doc_sei_despacho').type('1111')
+        cy.get('#nr_doc_sei_despacho').type('2222')
 
         cy.get('#submit').click()
+        cy.wait(2000)
         cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Continuar').click()
+        cy.wait(2000)
         cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click()
+        cy.wait(2000)
+
+        cy.reload()
 
         //CADASTRAR REITERAÇÃO
 
@@ -171,6 +179,14 @@ describe('consultar_requerimentos', () => {
 
         cy.get('.btn-success').click()
 
+        // //Validar campos de texto
+        //
+        // cy.get(':nth-child(1) > .form-group > .control-label').should('contain',"R.A")
+        // cy.get(':nth-child(2) > .form-group > .control-label').should('contain',"Empreendimento")
+        // cy.get(':nth-child(3) > .form-group > .control-label').should('contain',"Tipo Requerimento")
+        // cy.get('.col-md-12 > :nth-child(1) > .control-label').should('contain',"Requerimentos")
+        // cy.get('tr > :nth-child(1)').should('contain',"Requerimentos Reiterados")
+
        //Adicionar primeiro requerimento
 
         cy.get('#select2-regiao_adm-container').click()
@@ -178,12 +194,16 @@ describe('consultar_requerimentos', () => {
         cy.wait(2000)
 
         cy.get('#select2-combo_empreendimento-container').click()
-        cy.get('.select2-results').find('li').contains('esmeralda-teste').click({ force: true })
+        cy.get('.select2-results').find('li').contains('Ambiental11').click({ force: true })
+
+        cy.wait(2000)
 
         cy.get('#select2-combo_tipo_requerimento-container').click()
         cy.get('.select2-results').find('li').contains('Nova Permissão').click({ force: true })
 
+        cy.wait(4000)
         cy.get('#select2-combo_requerimento-container').click()
+        cy.wait(2000)
         cy.get('.select2-results').find('li').first().click({ force: true })
 
         cy.get('#btn_adicionar').click()
@@ -195,17 +215,191 @@ describe('consultar_requerimentos', () => {
         cy.wait(2000)
 
         cy.get('#select2-combo_empreendimento-container').click()
-        cy.get('.select2-results').find('li').contains('esmeralda-teste').click({ force: true })
-
+        cy.get('.select2-results').find('li').contains('Ambiental11').click({ force: true })
+        cy.wait(2000)
         cy.get('#select2-combo_tipo_requerimento-container').click()
+        cy.wait(2000)
+        cy.get('.select2-results').find('li').contains('Nova Permissão').click({ force: true })
+        cy.wait(2000)
+        cy.get('#select2-combo_requerimento-container').click()
+        cy.wait(2000)
+        cy.get('.select2-results').find('li').eq(1).click({ force: true })
+        cy.wait(2000)
+        cy.get('#btn_adicionar').click()
+        cy.wait(2000)
+        cy.get('#submit').should('be.visible').and('contain.text', 'Salvar').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Salvar').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click()
+        cy.wait(2000)
+
+        //PESQUISAR REITERAÇÃO DE REQUERIMENTO
+
+        // //Validar Campos de texto
+        // cy.get(':nth-child(1) > .form-group > .control-label').should('contain',"R.A")
+        // cy.get(':nth-child(2) > .form-group > .control-label').should('contain',"Empreendimento")
+        // cy.get(':nth-child(2) > .form-group > .control-label').should('contain',"Tipo Requerimento")
+        // cy.get('[aria-label="Código  Reiteração: Ordenar colunas de forma ascendente"]').should('contain',"Código Reiteração")
+        // cy.get('[aria-label="Empreendimento: Ordenar colunas de forma ascendente"]').should('contain',"Empreendimento")
+        // cy.get('[aria-label="Tipo Requerimento: Ordenar colunas de forma ascendente"]').should('contain',"Tipo Requerimento")
+        // cy.get('[aria-label="Data Publicação/Requerimento: Ordenar colunas de forma ascendente"]').should('contain',"Data Publicação/Requerimento")
+        // cy.get('thead > tr > .text-center').should('contain',"Ação")
+
+        cy.get('#select2-cd_regiao_admin-container').click()
+        cy.get('.select2-results').find('li').contains('RA-I - BRASILIA ').click({ force: true })
+
+        cy.get('#select2-tp_requerimento-container').click()
         cy.get('.select2-results').find('li').contains('Nova Permissão').click({ force: true })
 
-        cy.get('#select2-combo_requerimento-container').click()
-        cy.get('.select2-results').find('li').first().click({ force: true })
+        cy.get('#pesquisar').click()
 
-        cy.get('#btn_adicionar').click()
+        cy.get('#table-reiteracao_filter > label > .form-control').type('EcoConsultoria')
+
+        cy.get('a[title="Visualizar"]').first().click()
+
+        //Visualizar Reiteração
+
+        cy.get('[width="50%"]').should('contain',"Requerimentos Reiterados")
+
+        cy.get('[width="20%"]').should('contain',"Data de Publicação")
+
+        cy.get('[width="30%"]').should('contain',"Ação Requerimento")
+
+        cy.get('.form-group > :nth-child(1) > a').click()
+        cy.wait(2000)
+        cy.get('[onclick="window.history.back()"]').should('be.visible').and('contain.text', 'Voltar').click()
+        cy.wait(2000)
+        cy.get(':nth-child(1) > :nth-child(3) > a > .fa').click()
+        cy.wait(2000)
+        cy.get('[onclick="window.history.back()"]').should('be.visible').and('contain.text', 'Voltar').click()
+        cy.wait(2000)
+        cy.get('[onclick="window.history.back()"]').should('be.visible').and('contain.text', 'Voltar').click()
+
+        //CRIAR TERCEIRO REQUERIMENTO PARA EDITAR REITERAÇÃO
+
+
+        //PESQUISAR EMPREENDIMENTO
+
+        cy.get('.sidebar-menu > :nth-child(2) > a > span').click()
+
+        cy.get('.col-md-6 > .form-group > .select2-container > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').click()
+        cy.get('.select2-results').find('li').contains('Ambiental11').click({ force: true })
+        cy.get('#pesquisar').click()
+        cy.wait(2000)
+        cy.get('a[title="Visualizar"]').first().click();
+
+        cy.get('.requerimentos-aba').click()
+
+        //Cadastro
+        cy.get('#requerimento > .box > .box-body > .panel > .panel-body > .panel-footer > .btn-toolbar > .btn-success').click()
+
+        cy.get('[id^="select2-tp_demanda_permissao-"][id$="-container"]').click();
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('LP - Licença Prévia').click({ force: true });
+
+        cy.get('#nr_oficio').type('255151')
+
+        cy.get('#aa_oficio').type('2009')
+
+        cy.get('[id^="select2-cd_orgao_oficio-"][id$="-container"]').click();
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('ALMOX - ALMOXARIFADO').click({ force: true });
+
+        cy.get('#ds_observacao_doc_oficio').type('2054648')
+
+        cy.get('#select2-cd_pessoa_titular-container').click()
+        cy.get('.select2-results').find('li').contains(' Gabriel Tenorio Ramos').click({ force: true })
+
+        cy.get('#ds_requerimento').type('TESTANDO')
+
+        cy.get('[id^="select2-cd_orgao_requerimento-"][id$="-container"]').click();
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('ANA - SAF - AGENCIA NACIONAL DAS AGUAS').click({ force: true });
+        cy.wait(2000)
+        cy.get('#dt_publicacao').type('2021-09-01')
+
+        cy.get('[id^="select2-tp_status-"][id$="-container"]').click();
+        cy.wait(2000)
+        cy.get('.select2-results').should('be.visible').find('li.select2-results__option').should('have.length.gt', 0).contains('Indeferido').click({ force: true });
+        cy.wait(2000)
+        cy.get('#cd_orgao_processo_sei').type('25')
+        cy.get('#nr_processo_sei').type('2987')
+        cy.get('#aa_processo_sei').type('2020')
+
+        cy.get('#fl_reserva_orcamentaria').click()
+        cy.wait(1000)
+        cy.get('#nr_doc_sei_despacho').type('221111')
 
         cy.get('#submit').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Continuar').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click()
+        cy.wait(2000)
+
+        //Editar Reiteração
+
+        cy.get(':nth-child(7) > [href="#"]').click()
+
+        cy.get('.menu-open > .treeview-menu > :nth-child(2) > a > span').click()
+
+        cy.get('#select2-cd_regiao_admin-container').click()
+        cy.get('.select2-results').find('li').contains('RA-I - BRASILIA ').click({ force: true })
+
+        cy.get('#select2-tp_requerimento-container').click()
+        cy.get('.select2-results').find('li').contains('Nova Permissão').click({ force: true })
+
+        cy.get('#pesquisar').click()
+
+        cy.get('a[title="Editar"]').first().click()
+
+
+        // //validando campos de texto
+        // cy.get('.col-md-4 > .form-group > .control-label').should('contain',"R.A")
+        // cy.get('.col-md-5 > .form-group > .control-label').should('contain',"Empreendimento")
+        // cy.get('.col-md-3 > .form-group > .control-label').should('contain',"Tipo Requerimento")
+        // cy.get('.col-md-12 > :nth-child(1) > .control-label').should('contain',"Requerimentos")
+        // cy.get('thead > tr > :nth-child(1)').should('contain',"Requerimentos Reiterados")
+
+        cy.get('#select2-combo_requerimento-container').click()
+        cy.get('.select2-results').first().click()
+
+        //testando editar
+        cy.get('#select2-combo_requerimento-container').click()
+        cy.wait(1000)
+        cy.get('.select2-results').first().click()
+        cy.scrollTo('bottom')
+        cy.get('body').click(50, 50, { force: true })
+
+
+        cy.get('#btn_adicionar').click()
+        cy.wait(2000)
+        cy.get('#submit').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Continuar').click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click()
+        cy.wait(2000)
+        //Excluir reiteração
+        cy.get('a[title="Excluir"]').first().click()
+        cy.wait(2000)
+        cy.get('.modal-footer > .btn-primary').should('be.visible').and('contain.text', 'Confirmar').click()
+        cy.wait(2000)
+        cy.get('[onclick="window.history.back()"]').should('be.visible').and('contain.text', 'Voltar').click()
+        cy.wait(2000)
+
+        //Excluir
+
+        cy.get('#select2-cd_regiao_admin-container').click()
+        cy.get('.select2-results').find('li').contains('RA-I - BRASILIA ').click({ force: true })
+
+        cy.get('#select2-tp_requerimento-container').click()
+        cy.get('.select2-results').find('li').contains('Nova Permissão').click({ force: true })
+
+        cy.get('#pesquisar').click()
+
+        cy.get('a[title="Excluir"]').first().click()
+
+
+
 
     })
 })
