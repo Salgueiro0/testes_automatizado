43579c50c42d19c5f6a8fb36f6abb32bd4aab623
vitoria.gomes <vitoria.gomes@engesoftware.com.br>
2025-07-24 13:06:19 -0300

    Finalização de pagamentos

--- cypress/e2e/GLA/exigencias.cy.js
+++ cypress/e2e/GLA/exigencias.cy.js
@@ -1,18 +1,24 @@
-describe('consultar_requerimentos', () => {
 
-    beforeEach(() => {
+/// <reference types="cypress" />
 
-        cy.session('loginSession', () => {
-            cy.visit('http://gla-homol.terracapnet.local')
-            cy.viewport(2000, 1200)
-            cy.get(':nth-child(2) > .form-control').type('C9020840')
-            cy.get(':nth-child(3) > .form-control').type('welcome_1')
-            cy.get('.btn').click()
-            cy.get('h1').should('have.text', "Pendência")
-        })
-    })
-    it('Exigências', () => {
+describe('pagamento', () => {
 
+    it('teste tela de pagamentos', () => {
 
+        //LOGIN
+
+        cy.visit('http://gla-homol.terracapnet.local')
+        cy.viewport(1980, 1200)
+        cy.get(':nth-child(2) > .form-control').type('C9020840')
+        cy.get(':nth-child(3) > .form-control').type('welcome_1')
+        cy.get('.btn').click()
+        cy.get('h1').should('have.text', "Pendência")
     })
 })
+
+
+
+
+
+
+
