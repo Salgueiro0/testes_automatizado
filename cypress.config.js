f1287f1be9f98f9770ccd5e702e2fb883b1461d8
vitoria.gomes <vitoria.gomes@engesoftware.com.br>
2025-08-28 12:18:37 -0300

    refatoração de demandas_associacao

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout: 60000,
     baseUrl:'https://licenciamento-ambiental-homol.terracap.df.gov.br/login',
     // defaultCommandTimeout: 60000,
     //pageLoadTimeout: 60000,
--- cypress/support/pages/demandas_associacao/index.js
+++ cypress/support/pages/demandas_associacao/index.js
@@ -1,145 +1,52 @@
 const el = require('./elements').ELEMENTS
 class demandas_associacao {
 
-    irParaGLA() {
-        cy.visit('/');
-    }
-
+    irParaGLA() {cy.visit('/');}
     login() {
         cy.get(el.campoUsuario).type('C9020840');
         cy.get(el.campoSenha).type('welcome_1');
         cy.get(el.botaoLogin).click();
     }
-    validarTextoPendencia(){
-        cy.get('h1').should('have.text', "Pendência")
-    }
-    irParaDemandas(){
-        cy.get(':nth-child(3) > [href="#"]').click()
-    }
-    irParaDemandasAssociacao(){
-        cy.get('.menu-open > .treeview-menu > :nth-child(2) > a > span').click()
-    }
-    validarAbaAssocDemanda(){
-        cy.get('h1').should('have.text', "Associação de Demanda")
-    }
-    validarAbaAssocPesquisar(){
-        cy.get('.box-title').should('have.text', "Pesquisar")
-    }
-    validarAbaAssocTipoAssoc(){
-        cy.get('.col-md-4 > .form-group > .control-label').should('have.text', "Tipo de Associação")
-    }
-    validarAbaAssocRA(){
-        cy.get(':nth-child(2) > :nth-child(1) > .form-group > .control-label').should('have.text', "R.A")
-    }
-    validarAbaAssocEmpreendimento(){
-        cy.get(':nth-child(2) > .form-group > .control-label').should('have.text', "Empreendimento")
-    }
-    seletorTipo() {
-        cy.get('.col-md-4 > .form-group > .select2-container > .selection > .select2-selection').click()
-    }
-    seletorDropdown(opcao) {
-        cy.get('.select2-results').find('li').contains(opcao).click({force: true})
-        // cy.get('.select2-results')
-        //     .should('be.visible')          // espera até aparecer
-        //     .find('li')
-        //     .contains(opcao)
-        //     .click({force: true})
-    }
-    seletorRegiaoAdmin() {
-        cy.get('#select2-cd_regiao_admin-container').click()
-    }
-    seletorEmpreendimento() {
-        cy.get('#select2-cd_empreendimento-container > .select2-selection__clear').click()
-    }
-    limparFormularioPesquisa() {
-        cy.get('[onclick="limparFormularioPesquisa()"]').click()
-    }
-    clicarBotaoNovoCadastro(){
-        cy.get('.btn-success').click()
-    }
-    validarNovoCadasLabelNovoCadas(){
-        cy.get('.box-title').should('have.text', "Novo Cadastro")
-    }
-    validarNovoCadasLabelTipoAssoc(){
-        cy.get(':nth-child(1) > :nth-child(1) > .form-group > label').should('have.text', "Tipo de Associação *")
-    }
-    validarNovoCadasLabelRA(){
-        cy.get(':nth-child(1) > .form-group > .control-label').should('have.text', "R.A")
-    }
-    validarNovoCadasLabelEmpreendimento(){
-        cy.get(':nth-child(3) > .form-group > .control-label').should('have.text', "Empreendimento")
-    }
-    validarNovoCadasLabelDemandas(){
-        cy.get(':nth-child(4) > .form-group > label').should('have.text', "Demandas *")
-    }
-    validarNovoCadasTabelaRA(){
-        cy.get('tr > :nth-child(1)').should('have.text', "R.A")
-    }
-    validarNovoCadasTabelaEmpreendimento(){
-        cy.get('tr > :nth-child(2)').should('have.text', "Empreendimento")
-    }
-    validarNovoCadasTabelaDemandas(){
-        cy.get('tr > :nth-child(3)').should('have.text', "Demandas")
-    }
-    validarNovoCadasTabelaArquivada(){
-        cy.get('tr > :nth-child(4)').should('have.text', "Arquivada")
-    }
-    validarNovoCadasTabelaAcao(){
-        cy.get('.text-center').should('have.text', "Ação")
-    }
-    selecionarTpAssociacao(){
-        cy.get('#select2-tp_associacao-container').click()
-    }
-    seletorDemanda(){
-        cy.wait(2000)
-        cy.get('#select2-cd_demanda-container').click()
-    }
-    clicarBotaoAdicionar(){
-        cy.get('#btnAdicionar').click()
-    }
-    clicarExcluir(){
-        cy.get('.text-center > .fas').click()
-    }
-
-    seletorTpAssociacao(){
-        cy.get('#select2-tp_associacao-container').click()
-    }
-    seletorCdEmpreendimento(){
-        cy.get('#select2-cd_empreendimento-container').click()
-        cy.wait(2000)
-        cy.get('#select2-cd_empreendimento-container').click()
-        cy.wait(2000)
-    }
-    botaoSalvar(){
-        cy.get('#btnSalvar').click()
-    }
-    clicarModalOk(){
-        cy.get('.modal-footer > .btn').should('be.visible').and('contain.text', 'OK').click();
-    }
-    clicarModalBTNOk(){
-        cy.get('.modal-footer > .btn-primary').click()
-    }
-    clicarBotaoVoltar(){
-        cy.get('[onclick="window.history.back()"]').click()
-    }
-    clicarBotaoSalvar(){
-        cy.get('#btnSalvar').click()
-    }
-    clicarBotaoPesquisar(){
-        cy.get('#pesquisar').click()
-    }
-    clicarPrimeiroBotaoEditar(){
-        cy.get('a[title="Editar"]').first().click();
-    }
-    clicarAba(){
-        cy.get(':nth-child(2) > .text-center > .fas').click()
-    }
-    clicarPrimeiroBotaoVisualizar(){
-        cy.get('a[title="Visualizar"]').first().click();
-    }
-    botaoExcluirAssocDemand(){
-        cy.get('#btn-delete-associacao-demanda').click()
-    }
+    validarTextoPendencia(){cy.get(el.textoPendencia).should('have.text', "Pendência")}
+    irParaDemandas(){cy.get(el.abaDemandas).click()}
+    irParaDemandasAssociacao(){cy.get(el.menuDemandasAssociacao).click()}
+    validarAbaAssocDemanda(){cy.get(el.tituloAssocDemanda).should('have.text',"Associação de Demanda")}
+    validarAbaAssocPesquisar(){cy.get(el.tituloAssocPesquisar).should('have.text',"Pesquisar")}
+    validarAbaAssocTipoAssoc(){cy.get(el.labelAssocTipoAssoc).should('have.text',"Tipo de Associação")}
+    validarAbaAssocRA(){cy.get(el.labelAssocRA).should('have.text',"R.A")}
+    validarAbaAssocEmpreendimento(){cy.get(el.labelAssocEmpreendimento).should('have.text',"Empreendimento")}
+    seletorTipo(){cy.get(el.selectTpAssociacao).click();cy.get(el.opcaoSelect2).contains('Geral').click()}
+    clicaModalSalvar(){cy.get(el.botaoModalSalvar).click()}
+    seletorDropdown(opcao){cy.get(el.listaSelect2).find('li').contains(opcao).click({force:true})}
+    seletorRegiaoAdmin(){cy.get(el.selectRegiaoAdmin).click()}
+    seletorEmpreendimento(){cy.get(el.limparEmpreendimento).click()}
+    limparFormularioPesquisa(){cy.get(el.botaoLimparForm).click()}
+    clicarBotaoNovoCadastro(){cy.get(el.botaoNovoCadastro).click()}
+    validarNovoCadasLabelNovoCadas(){cy.get(el.tituloAssocPesquisar).should('have.text', "Novo Cadastro")}
+    validarNovoCadasLabelTipoAssoc(){cy.get(el.labelNovoTipoAssoc).should('have.text',"Tipo de Associação *")}
+    validarNovoCadasLabelRA(){cy.get(el.labelNovoRA).should('have.text',"R.A")}
+    validarNovoCadasLabelEmpreendimento(){cy.get(el.labelNovoEmpreendimento).should('have.text',"Empreendimento")}
+    validarNovoCadasLabelDemandas(){cy.get(el.labelNovoDemandas).should('have.text',"Demandas *")}
+    validarNovoCadasTabelaRA(){cy.get(el.tabelaRA).should('have.text',"R.A")}
+    validarNovoCadasTabelaEmpreendimento(){cy.get(el.tabelaEmpreendimento).should('have.text',"Empreendimento")}
+    validarNovoCadasTabelaDemandas(){cy.get(el.tabelaDemandas).should('have.text',"Demandas")}
+    validarNovoCadasTabelaArquivada(){cy.get(el.tabelaArquivada).should('have.text',"Arquivada")}
+    validarNovoCadasTabelaAcao(){cy.get(el.tabelaAcao).should('have.text',"Ação")}
+    selecionarTpAssociacao(){cy.get(el.selectTpAssociacaocont).click()}
+    seletorDemanda(){cy.wait(2000);cy.get(el.selectDemanda).click()}
+    clicarBotaoAdicionar(){cy.get(el.botaoAdicionar).click()}
+    clicarExcluir(){cy.get(el.botaoExcluir).click()}
+    seletorTpAssociacao(){cy.get(el.selectTpAssociacao).click()}
+    seletorCdEmpreendimento(){cy.get(el.selectCdEmpreendimento,{timeout:10000}).should('be.visible').click()}
+    botaoSalvar(){cy.get(el.botaoSalvar).click()}
+    clicarModalOk(){cy.get(el.botaoModalOk).should('be.visible').and('contain.text','OK').click()}
+    clicarModalBTNOk(){cy.get(el.botaoModalBTNOk).click()}
+    clicarBotaoVoltar(){cy.get(el.botaoVoltar).click()}
+    clicarBotaoSalvar(){cy.get(el.botaoSalvar).click()}
+    clicarBotaoPesquisar(){cy.get(el.botaoPesquisar).click()}
+    clicarPrimeiroBotaoEditar(){cy.get(el.primeiroBotaoEditar).first().click()}
+    clicarPrimeiroBotaoVisualizar(){cy.get(el.primeiroBotaoVisualizar).first().click()}
+    botaoExcluirAssocDemand(){cy.get(el.botaoExcluirAssocDemand).click()}
 }
 
 export default new demandas_associacao ()
